3b5a7c1e10cbbffc52ca06f4bc950c97fa183ec5
Lucas132005 <eguibo2@gmail.com>
2026-04-13 09:56:36 +0200

    Ejercicio 1 hecho

--- 2d/ej2d1.py
+++ 2d/ej2d1.py
@@ -49,10 +49,16 @@ Exemple:
 
 
 def kg_to_lb(kg):
-    # Write here your code
-    pass
-
+    if not isinstance(kg,(int,float)):
+        raise TypeError
+    elif kg <= 0:
+        raise ValueError
+    elif kg > 0:
+        libras = kg * 2.20
+        return libras
+    else:
+        raise Exception
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-#print(kg_to_lb(50))
+#print(kg_to_lb(kgr))
